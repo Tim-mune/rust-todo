deb34ce06901a64d65304b9c49b718115ca6f8e9
Timmy <neshtimo38@gmail.com>
2023-09-14 10:12:07 +0300

    fix: refactor models to allow serialization and deserialization

--- src/models.rs
+++ src/models.rs
@@ -1,6 +1,30 @@
+use actix_web::web;
+use serde::{Deserialize, Serialize};
 
-
+// #[derive(Debug,Serialize,Deserialize)]
+// enum TaskState {
+//     completed,
+//     pending,
+// }
+#[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Todo {
-    name: String,
-
+    pub id:i32,
+    pub name: String,
+    pub description: String,
+    pub completed: bool,
+    // status:TaskStat
+}
+impl From<web::Json<Todo>> for Todo {
+    fn from(value: web::Json<Todo>) -> Self {
+        // let task_status=match value.status {
+        //     TaskState::completed=>TaskState::completed,
+        //     TaskState::pending=>TaskState::pending
+        // };
+        Todo {
+            id:value.id,
+            name: value.name.clone(),
+            completed: value.completed,
+            description: value.description.clone(),
+        }
+    }
 }
